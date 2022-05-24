17d60ebfbfcc75750c4ebe539c9c10ac75d80f8b
Danilo Galvão <danilobispogalvao@gmail.com>
2022-05-24 11:39:08 -0300

    Updated models

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var http = require('http');
 const { API_KEY } = require('./apikey');
-const { SignatureSessionRestCore } = require('./lib/signature-session-rest-core');
+const { SignatureSessionRestCore : SignatureSessionRestCore } = require('./lib/signature-session-rest-core');
 const { RestPKICoreClient: RestPKICoreClient } = require('./lib/restpkicore-client');
-const {DocumentSummary : DocumentSummary} = require('./DocumentSummary')
+const { DocumentSummary: DocumentSummary } = require('./lib/document-summary')
+const { SignatureSessionRequestModel: SignatureSessionRequestModel} = require('./lib/signature-session-rest-core-model');
 
 // Live server, not needed for now 
 // http.createServer(function (req, res) {
@@ -15,12 +16,15 @@ var client = new RestPKICoreClient('https://core.pki.rest/', API_KEY, null, null
 async function testLog(){
   this._signSession = new SignatureSessionRestCore(client);
   const response =  await this._signSession.getSignatureSession('3fa85f64-5717-4562-b3fc-2c963f66afa6');
-  console.log(response);
+  // console.log(response);
 }
 var requestSignature = {
   'documentMetadata': 5,
   'enableBackgroundProcessing': true
 }
+
+var signatureRequestModel = new SignatureSessionRequestModel(requestSignature);
+console.log(signatureRequestModel._documentMetadata);
 
 console.log(requestSignature['enableBackgroundProcessing']);
 console.log(requestSignature['documentMetadata']);
